302751db483b5fe6f75851bc24d6e85e2b1fbb6f
parnevik <parnevik@deloitte.com>
2016-10-29 10:03:22 -0500

    Initial Upload

---
+++ [DD] Testing 123/Paul's User Event Script.js
@@ -0,0 +1,20 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       29 Oct 2016     parnevik
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @param {String} type Operation types: create, edit, view, copy, print, email
+ * @param {nlobjForm} form Current form
+ * @param {nlobjRequest} request Request object
+ * @returns {Void}
+ */
+function userEventBeforeLoad(type, form, request){
+ 
+}
